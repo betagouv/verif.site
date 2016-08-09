0046346fc7b4abb7019725c80037a490c7387c21
Thibaut Géry <thibaut.gery@gmail.com>
2016-08-09 15:05:27 +0200

    add qualys note in footer

--- front/src/Footer.js
+++ front/src/Footer.js
@@ -7,6 +7,9 @@ class Header extends Component {
         <p>
           Fait avec ♥ par&nbsp;<a href="https://beta.gouv.fr/" rel="noopener" target="_blank">l'Incubateur de services numériques</a>
         </p>
+        <p id="qualys">
+          L'API&nbsp;<a href="https://www.ssllabs.com/">Qualys SSL Labs</a>&nbsp;est utilisée pour calculer les notes SSL
+        </p>
       </div>
     );
   }
