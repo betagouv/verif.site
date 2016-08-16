e3905c00b859286d32e7e53403274de1340bdf8e
Florian Delezenne <del.florian@gmail.com>
2016-08-16 16:48:32 +0200

    Better date format

--- front/src/App.js
+++ front/src/App.js
@@ -11,13 +11,14 @@ class App extends Component {
   }
 
   getAnalytics() {
-    return fetch(`https://raw.githubusercontent.com/sgmap/sites/master/data/sites.json`)
+    return fetch(`https://raw.githubusercontent.com/sgmap/verif.site/0a747435e34fa03606c71523cb64dd45e66d4687/data/sites.json`)
       .then((response) => response.json())
       .then((json) => {
-        const analytics = Object.keys(json).map((key) => {
-          return json[key]
+        const analytics = Object.keys(json.data).map((key) => {
+          return json.data[key]
         })
-        this.setState({analytics})
+
+        this.setState({analytics, meta: json.meta})
       })
       .catch((err) => {
         console.error(err);
@@ -30,10 +31,10 @@ class App extends Component {
         <Header />
         <table className="site-table">
           <tbody>
-            {this.state ? this.state.analytics.data.map((site, idx) => <SiteRow key={idx} site={site} />) : 'Loading'}
+            {this.state ? this.state.analytics.map((site, idx) => <SiteRow key={idx} site={site} />) : 'Loading'}
           </tbody>
         </table>
-        <Footer lastUpdated={this.state.analytics.meta.lastUpdated} />
+        <Footer lastUpdated={this.state ? this.state.meta.lastUpdated : '...'} />
       </div>
     );
   }
