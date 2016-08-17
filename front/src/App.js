0788c7f5c7cac9d880ffaea4eca9c29c1e834af9
Thibaut Géry <thibaut.gery@gmail.com>
2016-08-17 18:21:36 +0200

    fix date

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,7 @@ import Footer from './Footer'
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {analytics: []};
+    this.state = {analytics: [], meta: {}};
     this.getAnalytics();
   }
 
