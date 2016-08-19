57b57777793327758d6c0cd47c6fca1b66156201
tmerlier <tmerlier@student.42.fr>
2016-08-19 16:57:27 +0200

    Remove useless condition

--- front/src/Content.js
+++ front/src/Content.js
@@ -6,13 +6,11 @@ class Content extends Component {
     var result = []
     var search = this.props.search
 
-    if (this.props.sites.length) {
-      this.props.sites.forEach(function(e) {
-        if (e.meta.Administration.toLowerCase().match(search) || e.meta.Domain.toLowerCase().match(search)) {
-          result.push(e)
-        }
-      })
-    }
+    this.props.sites.forEach(function(e) {
+      if (e.meta.Administration.toLowerCase().match(search) || e.meta.Domain.toLowerCase().match(search)) {
+        result.push(e)
+      }
+    })
 
     return (
       <table className="site-table">
