8d7d6ae2139f61622cd99ed42330d34ec02855dc
tmerlier <tmerlier@student.42.fr>
2016-08-22 15:43:16 +0200

    Remove useless div

--- front/src/SearchBar.js
+++ front/src/SearchBar.js
@@ -3,14 +3,12 @@ import React, { Component } from 'react'
 class SearchBar extends Component {
   render() {
     return (
-      <div>
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Recherche"
-            value={this.props.text}
-            onChange={this.props.onChange} />
-        </div>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Recherche"
+          value={this.props.text}
+          onChange={this.props.onChange} />
       </div>
     );
   }
