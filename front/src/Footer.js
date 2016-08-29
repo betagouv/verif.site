483bea499143db031dc7de58603de49aaedfefee
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-08-29 11:58:32 +0200

    Decrease importance of last update

--- front/src/Footer.js
+++ front/src/Footer.js
@@ -12,7 +12,7 @@ class Header extends Component {
           <li>L'API&nbsp;<a href="https://www.ssllabs.com/">Qualys SSL Labs</a>&nbsp;est utilisée pour calculer les notes SSL.</li>
           <li>Icône de cadenas par <a href="https://design.google.com/icons/#ic_lock_outline">Google</a>.</li>
         </ul>
-        <p>Dernière mise à jour le {this.props.lastUpdated}</p>
+        <p><small>Dernière mise à jour le {this.props.lastUpdated}.</small></p>
       </footer>
     )
   }
