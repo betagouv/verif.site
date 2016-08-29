d0853bdbfc6689f31379fad58e0eceb942a8f626
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-08-29 11:58:43 +0200

    Remove unused id

--- front/src/Footer.js
+++ front/src/Footer.js
@@ -8,7 +8,7 @@ class Header extends Component {
         <p>
           Fait avec ♥ par&nbsp;<a href="https://beta.gouv.fr/" rel="noopener" target="_blank">l'Incubateur de services numériques</a>
         </p>
-        <ul id="credits">
+        <ul>
           <li>L'API&nbsp;<a href="https://www.ssllabs.com/">Qualys SSL Labs</a>&nbsp;est utilisée pour calculer les notes SSL.</li>
           <li>Icône de cadenas par <a href="https://design.google.com/icons/#ic_lock_outline">Google</a>.</li>
         </ul>
