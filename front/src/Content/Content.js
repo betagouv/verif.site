1c1b1bc80ba6ba3b38a10cbf2d84873f7cfd9b40
tmerlier <tmerlier@student.42.fr>
2016-08-30 12:00:42 +0200

    Change let to const

--- front/src/Content/Content.js
+++ front/src/Content/Content.js
@@ -7,7 +7,7 @@ import './Content.css'
 function getParameterByName(name, url) {
   if (!url) url = window.location.href
   name = name.replace(/[\[\]]/g, "\\$&")
-  let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
       results = regex.exec(url)
   if (!results) return null
   if (!results[2]) return ''
