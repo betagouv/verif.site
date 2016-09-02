767eb071166ef6ff5bd879d9eeb57cb41e54cbc0
Florian Delezenne <del.florian@gmail.com>
2016-09-02 17:04:05 +0200

    High level badges

--- front/src/Badges/Badges.js
+++ front/src/Badges/Badges.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import HttpsValid from '../cells/https/Valid/Valid'
 import HttpsGrade from '../cells/https/Grade/Grade'
-import HttpsEnforce from '../cells/https/Enforce/Enforce'
 import HttpsDate from '../cells/https/Date/Date'
-import Http2 from '../cells/Http2/Http2'
 import CustomUptimeRatio from '../cells/uptime/CustomUptimeRatio/CustomUptimeRatio'
-import AllTimeUptimeRatio from '../cells/uptime/AllTimeUptimeRatio/AllTimeUptimeRatio'
 import './Badges.css'
 
 const Badges = ({site, monitor}) =>
   <div className='badges'>
     <HttpsValid inspect={site.inspect} />
-    <HttpsEnforce inspect={site.inspect} />
     <HttpsGrade tls={site.tls} />
-    <Http2 tls={site.tls} />
     <HttpsDate sslyze={site.sslyze} />
     <CustomUptimeRatio monitor={monitor} />
-    <AllTimeUptimeRatio monitor={monitor} />
   </div>
 
 export default Badges
--- front/src/HeaderRow/HeaderRow.js
+++ front/src/HeaderRow/HeaderRow.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import HeaderTitle from '../HeaderTitle/HeaderTitle'
 import Badges from '../Badges/Badges'
+import MiniBadges from '../MiniBadges/MiniBadges'
 import './HeaderRow.css'
 
 const HeaderRow = (props) =>
   <div className='site-header'>
     <HeaderTitle {...props} />
-    <Badges {...props} />
+    <div className='badges-container'>
+      <Badges {...props} />
+      <MiniBadges {...props} />
+    </div>
   </div>
 
 export default HeaderRow
