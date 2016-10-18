6f79a945e2922b29ba777e23bf85c0b5f29504ba
Florian Delezenne <del.florian@gmail.com>
2016-10-18 16:28:31 +0200

    Move docker run to circle-ci

--- import/index.js
+++ import/index.js
@@ -1,4 +1,3 @@
-const analyse = require('./analyze')
 const model = require('./model')
 const request = require('superagent');
 const fs = require('fs')
@@ -7,39 +6,28 @@ const apiKey = process.env.DATA_GOUV_API_KEY;
 
 const scanners = ['inspect', 'sslyze', 'tls', 'pageload']
 const name = 'sites'
-const options = {
-  dockerImage: 'betagouv/domain-scan',
-  scanners,
-  file: `${name}.csv`,
-}
 
-analyse(options, (err, stdout, stderr) => {
-  if(err) return console.error(err)
+const files = scanners.map((item) => {
+  return {name: item, path: `${__dirname}/../results/${item}.csv`}
+})
 
-  console.log('analyze Done')
+files.push({name: 'meta', path: `${__dirname}/../data/${name}.csv`})
 
-  const files = scanners.map((item) => {
-    return {name: item, path: `${__dirname}/../results/${item}.csv`}
-  })
+model(files, (data) => {
+  const filePath = `${__dirname}/../data/${name}.json`;
+  fs.writeFileSync(filePath, JSON.stringify({data, meta: {lastUpdated: new Date().toLocaleString()}}, null, 2), 'utf8')
 
-  files.push({name: 'meta', path: `${__dirname}/../data/${name}.csv`})
+  if (!process.env.DATA_GOUV_API_KEY) {
+    return console.log('data.gouv api key not found, finished');
+  }
 
-  model(files, (data) => {
-    const filePath = `${__dirname}/../data/${name}.json`;
-    fs.writeFileSync(filePath, JSON.stringify({data, meta: {lastUpdated: new Date().toLocaleString()}}, null, 2), 'utf8')
-
-    if (!process.env.DATA_GOUV_API_KEY) {
-      return console.log('data.gouv api key not found, finished');
-    }
-
-    request
-      .post('http://www.data.gouv.fr/api/1/datasets/5805f1e2c751df2bb879df72/resources/234299a6-e9a4-4cf4-a220-681a94ac3e92/upload/')
-      .attach('file', filePath)
-      .set('X-API-KEY', apiKey)
-      .set('Accept', 'application/json')
-      .end(function(err, res){
-        if (err) return console.err(err);
-        console.log('successfully uploaded to data.gouv');
-      })
-  })
+  request
+    .post('http://www.data.gouv.fr/api/1/datasets/5805f1e2c751df2bb879df72/resources/234299a6-e9a4-4cf4-a220-681a94ac3e92/upload/')
+    .attach('file', filePath)
+    .set('X-API-KEY', apiKey)
+    .set('Accept', 'application/json')
+    .end(function(err, res){
+      if (err) return console.err(err);
+      console.log('successfully uploaded to data.gouv');
+    })
 })
