18856db7f884313b2fc9fce9e0178e8a9f37f5fb
Florian Delezenne <del.florian@gmail.com>
2017-01-10 11:40:31 +0100

    Use store in tests

--- src/containers/App-test.js
+++ src/containers/App-test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { expect } from 'chai'
-import { shallow, mount } from 'enzyme'
-import sinon from 'sinon'
+import { mount } from 'enzyme'
 import App from './App'
 import Header from '../components/Header/Header'
 import Content from '../components/Content/Content'
@@ -9,6 +8,8 @@ import Site from '../components/Site/Site'
 import Footer from '../components/Footer/Footer'
 import fetchMock from 'fetch-mock'
 import { Loader } from 'react-loaders'
+import { createMockStore } from 'redux-test-utils';
+import { Provider } from 'react-redux';
 
 const httpSites = require('../../test/resources/http-sites')
 const arraySite = require('../../test/resources/array-sites')
@@ -16,7 +17,7 @@ const arraySite = require('../../test/resources/array-sites')
 const resourceUrl = 'https://www.data.gouv.fr/s/resources/caracteristiques-techniques-des-sites/234/sites.json'
 const datasetUrl = 'https://www.data.gouv.fr/api/1/datasets/5805f1e2c751df2bb879df72/'
 
-describe("Apps", () => {
+describe('Apps', () => {
 
   before(() => {
     return fetchMock.get(resourceUrl, httpSites, {name: 'resourceMock'})
@@ -28,114 +29,49 @@ describe("Apps", () => {
 
   afterEach(fetchMock.reset)
 
-  describe("Init", () => {
+  describe('Init', () => {
+    it('Shows the loader', () => {
+      const store = createMockStore({ isFetching: true, items: [] })
+      const wrapper = mount(<Provider store={store}><App /></Provider>)
 
-    it('show the loader', () => {
-      const wrapper = shallow(<App/>)
-
-      expect(wrapper.state().loading).to.be.true
       expect(wrapper.find(Loader)).to.have.length(1)
-    })
-
-    it('Should add the analytics', (done) => {
-      //when
-      const wrapper = shallow(<App/>)
-
-      //then (sââââââââle)
-      setTimeout(() => {
-        expect(fetchMock.calls('datasetMock')).to.have.length(1)
-        expect(wrapper.state().analytics).to.have.length(2)
-        expect(wrapper.state().loading).to.be.false
-        expect(wrapper.state().analytics).deep.equal(arraySite)
-        done()
-      }, 10)
-    })
-
-    it('Should update state on fetch', (done) => {
-
-      //when
-      const wrapper = shallow(<App/>)
-
-      //then (sââââââââle)
-      setTimeout(() => {
-        expect(fetchMock.calls('datasetMock')).to.have.length(1)
-        expect(wrapper.state().meta.lastUpdated).to.equal('8/16/2016, 4:48:01 PM')
-        done()
-      }, 10)
     })
   })
 
-  describe("Contains", () => {
-    it("show the header", () => {
-      const wrapper = shallow(<App/>)
+  describe('Contains', () => {
+    it('Shows the header', () => {
+      const store = createMockStore({ items: arraySite })
+      const wrapper = mount(<Provider store={store}><App /></Provider>)
 
       expect(wrapper.find(Header)).to.have.length(1)
     })
 
-    it("show the content", () => {
-      const wrapper = shallow(<App/>)
+    it('Shows the content', () => {
+      const store = createMockStore({ items: arraySite })
+      const wrapper = mount(<Provider store={store}><App /></Provider>)
 
       expect(wrapper.find(Content)).to.have.length(1)
     })
 
-    it("show the footer", () => {
-      const wrapper = shallow(<App/>)
+    it('Shows the footer', () => {
+      const store = createMockStore({ items: arraySite })
+      const wrapper = mount(<Provider store={store}><App /></Provider>)
 
       expect(wrapper.find(Footer)).to.have.length(1)
     })
 
-    it('Should have 2 rows', (done) => {
+    it('Should have 2 rows', () => {
+      const store = createMockStore({ items: arraySite })
+      const wrapper = mount(<Provider store={store}><App /></Provider>)
 
-      //when
-      const wrapper = mount(<App/>)
-
-      //then (sââââââââle)
-      setTimeout(() => {
-        expect(wrapper.find(Site)).to.have.length(2)
-        done()
-      }, 200)
+      expect(wrapper.find(Site)).to.have.length(2)
     })
 
-    it('With seeking, it must display the corresponding single site', (done) => {
-      const wrapper = mount(<App />)
+    it('With query, it must display the corresponding single site', () => {
+      const store = createMockStore({ items: arraySite, query: 'bourse' })
+      const wrapper = mount(<Provider store={store}><App /></Provider>)
 
-      //then (sââââââââle)
-      setTimeout(() => {
-        wrapper.setState({query: 'bourse'})
-        expect(wrapper.find(Site)).to.have.length(1)
-        done()
-      }, 200)
+      expect(wrapper.find(Site)).to.have.length(1)
     })
-
-
-    it('should init query', (done) => {
-      const wrapper = mount(<App />)
-
-      expect(wrapper.state().query).equal('')
-      done()
-    })
-  })
-
-  it("calls handleTextChange with the correct query", () => {
-    const wrapper = mount(<App/>)
-    const textChangeSpy = sinon.spy();
-
-    wrapper.instance().handleTextChange = textChangeSpy
-    wrapper.instance().filterAdministration('test')
-
-    expect(textChangeSpy).to.have.been.called
-    expect(textChangeSpy.args[0][0]).to.equal('test');
-  })
-
-  it("calls handleTextChange with an empty query if it was already selected", () => {
-    const wrapper = mount(<App/>)
-    const textChangeSpy = sinon.spy();
-
-    wrapper.setState({query: 'bourse'})
-    wrapper.instance().handleTextChange = textChangeSpy
-    wrapper.instance().filterAdministration('bourse')
-
-    expect(textChangeSpy).to.have.been.called
-    expect(textChangeSpy.args[0][0]).to.equal('');
   })
 })
